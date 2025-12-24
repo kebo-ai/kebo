189a9eb9229acf04982f0b57a3ce95d5f07c0b0c
Cris <cristian.correa.cs@gmail.com>
2025-12-23 21:22:48 -0500

    feat(theme): add typography definitions

---
+++ src/theme/typography.ts
@@ -0,0 +1,7 @@
+export const customFontsToLoad = {
+  SFUIDisplayLight: require("../assets/fonts/SFUIDisplay-Light.otf"),
+  SFUIDisplayRegular: require("../assets/fonts/SFUIDisplay-Regular.otf"),
+  SFUIDisplayMedium: require("../assets/fonts/SFUIDisplay-Medium.ttf"),
+  SFUIDisplaySemiBold: require("../assets/fonts/SFUIDisplay-SemiBold.otf"),
+  SFUIDisplayBold: require("../assets/fonts/SFUIDisplay-Bold.otf"),
+}

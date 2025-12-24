9f072cfcbe7734b5600fde6061a8591029bf7c2e
Cris <cristian.correa.cs@gmail.com>
2025-12-23 21:30:41 -0500

    feat(components): add RecurrenceIconHomeSvg svg

---
+++ src/components/svg/RecurrenceIconHomeSvg.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Svg,
+  Path,
+  Circle,
+  G,
+  Defs,
+  ClipPath,
+  Rect,
+  Filter,
+  FeFlood,
+  FeBlend,
+  FeGaussianBlur,
+  Ellipse,
+} from "react-native-svg";
+import { colors } from "../../theme/colors";
+
+interface RecurrenceIconHomeSvgProps {
+  width?: number;
+  height?: number;
+  color?: string;
+  isPressed?: boolean;
+}
+
+export const RecurrenceIconHomeSvg: React.FC<RecurrenceIconHomeSvgProps> = ({
+  width = 28,
+  height = 28,
+  color = colors.primary,
+}) => (
+  <Svg width={width} height={height} viewBox="0 0 28 28" fill="none">
+    <Path
+      d="M6 14C6 9.58261 9.58261 6 14 6C17.2557 6 20.0591 7.94783 21.3043 10.7374M21.3043 7.3913V10.8696H17.8261M6.69565 17.2626C7.94087 20.0522 10.7443 22 14 22C18.4174 22 22 18.4174 22 14M6.69565 20.6087V17.1304H10.1739"
+      stroke={color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);

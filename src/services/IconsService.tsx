791cb8d8b0fbc9a50a5e2d824f56f002bf7b0259
Cris <cristian.correa.cs@gmail.com>
2025-12-23 21:28:24 -0500

    feat(services): add icons service

---
+++ src/services/IconsService.tsx
@@ -0,0 +1,11 @@
+import { supabase } from "../config/supabase";
+import logger from "../utils/logger";
+
+export const getIcons = async () => {
+  const { data, error } = await supabase.from("icons").select("*").eq("is_deleted", false);
+  if (error) {
+    logger.error("Error fetching icons:", error.message);
+    return null;
+  }
+  return data;
+};

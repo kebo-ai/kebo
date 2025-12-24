936519cfbe41013043789c8e061f142f5dcb6001
Camilo Barrera <cristian.correa.cs@gmail.com>
2025-12-23 21:23:16 -0500

    feat(config): add supabase client

---
+++ src/config/supabase.ts
@@ -0,0 +1,48 @@
+import { createClient } from "@supabase/supabase-js";
+import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import logger from "../utils/logger";
+
+const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
+
+const HybridStorageAdapter = {
+  getItem: async (key: string) => {
+    try {
+      const secureValue = await SecureStore.getItemAsync(key);
+      if (secureValue) return secureValue;
+      
+      return await AsyncStorage.getItem(key);
+    } catch (error) {
+      return null;
+    }
+  },
+  setItem: async (key: string, value: string) => {
+    try {
+      if (value.length < 2000) {
+        await SecureStore.setItemAsync(key, value);
+      } else {
+        await AsyncStorage.setItem(key, value);
+      }
+    } catch (error) {
+      logger.error('Error storing value:', error);
+    }
+  },
+  removeItem: async (key: string) => {
+    try {
+      await SecureStore.deleteItemAsync(key);
+      await AsyncStorage.removeItem(key);
+    } catch (error) {
+      logger.error('Error removing value:', error);
+    }
+  },
+};
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: HybridStorageAdapter,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});

b62a8be37f91bdc432f0ddae0fa61951a03887b1
Cris <cristian.correa.cs@gmail.com>
2025-12-23 21:22:17 -0500

    feat(theme): add color definitions

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+
+export const colors = {
+  primary: "#6934D2",
+  primaryBg: "rgba(105, 52, 210, 0.15)",
+  secondary: "#260035",
+  white: "#FFFFFF",
+  black: "#000000",
+  gray: "#747775",
+  textGray: "#606A84",
+  bgClear: "#F0EBFB",
+  bgGray: "#B7BBC7",
+  "border-gray": "#D8DADC",
+} as const

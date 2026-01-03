5624fed313e2f0ec986693556af2f3d457053e05
Mr uprizing <uprizingfaze@gmail.com>
2026-01-03 13:39:13 -0500

    Feat(Core): Upgrade NextJs to 16.1 & new optimizations

--- apps/web/src/components/logos/kebo.tsx
+++ apps/web/src/components/logos/kebo.tsx
@@ -1,6 +1,6 @@
-import type { LucideProps } from "lucide-react"
+import type { SVGProps } from 'react'
 
-export const KeboLogo = (props: LucideProps) => (
+export const KeboLogo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="32"

f41a90429b5bd90506962eba3a5d40422efb0cf1
Cris <cristian.correa.cs@gmail.com>
2026-02-23 00:57:10 -0500

    wip

--- apps/api/api/index.js
+++ apps/api/api/index.js
@@ -1,4 +1,4 @@
-import { handle } from "hono/vercel"
+import { getRequestListener } from "@hono/node-server"
 import app from "../dist/index.js"
 
-export default handle(app)
+export default getRequestListener((request) => app.fetch(request, process.env))
